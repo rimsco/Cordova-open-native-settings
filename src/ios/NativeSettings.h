5924134d3bf365e5310f128b8100b6b348b5ee38
phongnh <phongnguyen1902@gmail.com>
2016-06-03 15:03:27 +0700

    not working on iOS 9 #1

--- src/ios/NativeSettings.h
+++ src/ios/NativeSettings.h
@@ -8,6 +8,9 @@
 
 #import <Cordova/CDV.h>
 
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+
+
 @interface NativeSettings : CDVPlugin
 
 - (void)open:(CDVInvokedUrlCommand*)command;
--- src/ios/NativeSettings.m
+++ src/ios/NativeSettings.m
@@ -8,12 +8,39 @@
 
 #import "NativeSettings.h"
 
+
 @implementation NativeSettings
 
 - (void)open:(CDVInvokedUrlCommand*)command
 {
-        NSURL *appSettings = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
-        [[UIApplication sharedApplication] openURL:appSettings];
+
+    
+    NSString* appsetting = [command.arguments objectAtIndex:0];
+    NSString* urls = nil ;
+    CDVPluginResult* pluginResult = nil;
+
+    if(SYSTEM_VERSION_LESS_THAN(@"8.0")){
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+    }else{
+    
+    if([appsetting isEqualToString:@"wifi"]){
+        urls = @"prefs:root=WIFI";
+    }
+    else if([appsetting isEqualToString:@"cellular"]){
+        urls = @"prefs:root=MOBILE_DATA_SETTINGS_ID";
+    }else{
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Url is not correct or not available in current iOS version"];
+    }
+    
+    if(urls != nil){
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urls]];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Open setting sucess"];
+    }
+    else{
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Url is null"];
+    }
+    }
+    
 }
 
 @end
